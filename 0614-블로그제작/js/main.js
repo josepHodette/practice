bb4757bec84d596c30a1e928582af3688f914f9d
josepH <lyrickor@gmail.com>
2023-07-12 12:31:36 +0900

    Add files via upload

--- 0614-블로그제작/js/main.js
+++ 0614-블로그제작/js/main.js
@@ -22,4 +22,25 @@ $(document).ready(function(){
     $(".hover6").on("mousemove",function(){
         $(".main img").attr({"src":"img/waterfire.jpg"})
     });
+    
+    $(".bxslider").bxSlider({
+        mode:'horizontal', //가로방향
+        slideWidth:600, // 너비값
+        captions:true, //제목표시
+        auto:true,   //자동실행
+        autoControls:true,  //컨트롤버튼 재생 정지
+        stopAutoOnclick:true, //클릭해서 자동 실행 멈춤할 수 있도록 지정
+      });
+
+    $(".owl-carousel").owlCarousel({
+        items:3,//화면에 나올 항목 개수
+        margin:10,//항목과 항목 사이의 간격(margin-right)
+        loop:true,//무한반복
+        nav:true,//nav 표시
+        navText:['이전','이후'],//nav 이전, 다음 표시
+        autoplay:true,//자동스크롤 실행
+        autoplayTimeout:3000,//자동스크롤할 때 시간 간격, 밀리초
+        autoplayHoverPause:true,//내용위로 마우스 포인터를 올리면 자동 스크롤 일시멈춤
+
+    });
 });
