e5e2435d29134bd2bbc9eefccc384cd557725fe0
josepH <lyrickor@gmail.com>
2023-07-25 15:46:29 +0900

    Add files via upload

---
+++ 웹디자인기능사 실기/0725-서울구석구석/js/main1.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".navi>li").on("mousemove",function(){
+        $(this).find(".submenu").stop().slideDown(500);
+    });
+    $(".navi>li").on("mouseleave",function(){
+        $(this).find(".submenu").stop().slideUp(400);
+    });
+    var imgs=2;
+    var now=0;
+    start();
+    function start() {
+        $(".imgslide>a").eq(0).siblings().css({ "margin-left": "-1700px" });
+        setInterval(function () { slide(); }, 3000);
+    }
+    function slide() {
+        now = now == imgs ? 0 : now += 1;
+        $(".imgslide>a").eq(now - 1).css({ "margin-left": "-1700px" });
+        $(".imgslide>a").eq(now).css({ "margin-left": "0px" });
+    }
+    $(".notice li:first").click(function () {
+        $(".modal").addClass("active")
+    });
+    $(".btn").click(function () {
+        $(".modal").removeClass("active");
+    });
+});
